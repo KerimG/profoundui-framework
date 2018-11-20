c2501c8f55f058c7cea55b6185bfded694fe1229
Alex Roytman <aroytman@profoundlogic.com>
2018-11-20 13:03:28 -0800

    Social Sign In for NodeRun

--- proddata/js/designer/dspf/cloud/signin_screen.js
+++ proddata/js/designer/dspf/cloud/signin_screen.js
@@ -59,3 +59,45 @@ pui.cloud["signin screen"]["next"] = function() {
     }
   });
 }
+
+
+pui.cloud["signin screen"]["social"] = function(type) {
+  var w = 600;
+  var h = 600;
+  
+  if (type === "facebook") {
+    w = 1100;
+  }
+  
+  var url = "/oauth/" + type;
+  
+  // Calculate how to center window 
+  
+  // Fixes dual-screen position                         Most browsers      Firefox
+  var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : window.screenX;
+  var dualScreenTop = window.screenTop != undefined ? window.screenTop : window.screenY;
+
+  var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+  var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+  var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+  var top = ((height / 2) - (h / 2)) + dualScreenTop;
+  var newWindow = window.open(url, "Sign in with " + type, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+}
+
+
+pui.cloud["signin screen"]["complete social"] = function(info) {
+  pui["setCookie"]("pui-cloud-token", info["token"], 366, "/");  
+  pui.cloud.showUser(info);
+  if (pui.cloud.htmlDialogType === "publish" || pui.cloud.htmlDialogType === "fork") {
+    pui.cloud["publish screen"].show();
+  }
+  else {
+    var win = pui.cloud.savedHtmlDialogReference;
+    win.close();
+  }
+  pui.ide.refreshRibbon();
+  pui.social.refreshSocialData();
+  pui.social.getWorkspaceInfo();
+}
+
